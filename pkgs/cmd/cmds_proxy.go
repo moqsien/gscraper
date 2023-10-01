9f2bf8544b7e1861b3a42d00b22207b11724b59b
moqsien <moqsien@foxmail.com>
2023-10-02 01:02:11 +0800

    add new command

--- pkgs/cmd/cmds_proxy.go
+++ pkgs/cmd/cmds_proxy.go
@@ -59,4 +59,36 @@ func InitProxy() {
 			return nil
 		},
 	})
+
+	app.Add(&cli.Command{
+		Name:    "proxy-rawuri-only",
+		Aliases: []string{"ruo", "ro"},
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:        "use-ghproxy",
+				Aliases:     []string{"ghproxy", "g"},
+				Destination: &enableGHProxy,
+				Usage:       "use ghproxy.com to speedup.",
+			},
+			&cli.BoolFlag{Name: "use-proxy",
+				Aliases:     []string{"proxy", "p"},
+				Destination: &enableLocalProxy,
+				Usage:       "use local proxy to speedup."},
+		},
+		Usage: "Get rawURIs and geoinfo for neobox.",
+		Action: func(ctx *cli.Context) error {
+			runner := proxy.NewProxyRunner()
+			runner.AddSite(proxies.NewSubscribers())
+			runner.AddSite(proxies.NewWSZiwo())
+			runner.AddSite(proxies.NewFreeFQ())
+			if enableGHProxy {
+				os.Setenv(config.EnableGithubSpeedupEnvName, "1")
+			}
+			if enableLocalProxy {
+				os.Setenv(config.EnableProxyEnvName, "1")
+			}
+			runner.Run()
+			return nil
+		},
+	})
 }
